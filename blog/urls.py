8d588ed088cff70f88075b58cb8396229e3d7350
marceloagoston <marceloagoston91@gmail.com>
2019-11-14 10:53:48 -0300

    CRUD for Posts Done

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeteleView
 
 
 urlpatterns = [
+	path('post/<int:pk>/delete', BlogDeteleView.as_view(), name='post_delete'),
+	path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
 	path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), #todos los post van a estar en post/ y el resto es la clave primaria que identifica a cada uno
     path('', BlogListView.as_view(), name='home'),
