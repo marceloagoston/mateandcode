4933b630d4b91f2fdf13094548e62ef8df10373c
marceloagoston <marceloagoston91@gmail.com>
2019-11-15 11:00:19 -0300

    forms and user accounts

--- mateandcode/urls.py
+++ mateandcode/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cuentas/', include('django.contrib.auth.urls')),
+    path('cuentas/', include('accounts.urls')), #in this case the order matters. 'cause dj reads top-to-bot
     path('', include('blog.urls')),
 ]
